19624c8f6f0de43f498dab6476e36101e1ff174a
Ivor <ivor@devpie.io>
2019-09-04 17:44:24 +0200

    initial commit

--- config.js
+++ config.js
@@ -1,46 +1,35 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://ivorscott.netlify.com",
+  pathPrefix: "/",
+  title: "Art, Tech & Ideas",
+  subtitle: `I design, develop, and deploy Typescript Applications. 
+    Iterating through customer feedback to achieve product-market-fit for my organization and clients`,
+  copyright: "© All rights reserved.",
+  disqusShortname: "ivorscott",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: "About me",
+      path: "/pages/about"
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "Ivor Scott",
+    photo: "/photo.png",
+    bio: `I design, develop, and deploy.\n
+      Iterating through customer feedback to achieve product-market-fit.`,
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: ''
+      email: "ivor@devpie.io",
+      twitter: "https://twitter.com/ivorsco77",
+      github: "https://github.com/ivorscott"
     }
   }
 };
--- content/pages/about.md
+++ content/pages/about.md
@@ -4,4 +4,29 @@ title: About me
 slug: /pages/about
 draft: false
 ---
-Full stack developer
+
+I am a self-motivated full stack developer with two Fine Arts degrees leveraging my creativity in software. I communicate effectively and learn quick. I develop strong working relationships with others and enjoy what I do.
+
+### Where I've Worked
+
+Mass Mutual Financial Group, Siemens
+
+### Things I Use
+
+D3
+Scss
+Jest
+React
+NodeJs
+Docker
+Flexbox
+Cypress
+GraphQL
+GatsbyJs
+Wordpress
+Golang
+Javascript
+Typescript
+Firebase
+Digital Ocean
+Docker Swarm
