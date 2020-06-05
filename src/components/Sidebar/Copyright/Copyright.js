bd9ec476558e4b296f78e67f3de8f85969ad0a2c
Ivor <ivor@devpie.io>
2020-06-05 12:01:17 +0200

    feat: buy me a coffee

--- src/components/Sidebar/Copyright/Copyright.js
+++ src/components/Sidebar/Copyright/Copyright.js
@@ -1,14 +1,24 @@
 // @flow strict
-import React from 'react';
-import styles from './Copyright.module.scss';
+import React from "react";
+import styles from "./Copyright.module.scss";
 
 type Props = {
-  copyright: string
+  copyright: string,
 };
 
 const Copyright = ({ copyright }: Props) => (
-  <div className={styles['copyright']}>
+  <div className={styles["copyright"]}>
     {copyright}
+    <div className={styles["support"]}>
+      <a href="https://ko-fi.com/G2G01SD6G" target="_blank">
+        <img
+          height="36"
+          src="https://cdn.ko-fi.com/cdn/kofi3.png?v=2"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
+      </a>
+    </div>
   </div>
 );
 
